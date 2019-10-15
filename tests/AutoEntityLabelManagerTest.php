450088f6ca84f6e4664927b470f9609d59596958
alberto56 <albert@mediatribe.net>
2019-10-15 16:55:44 -0400

    Failing to test a protected method

--- tests/AutoEntityLabelManagerTest.php
+++ tests/AutoEntityLabelManagerTest.php
@@ -26,6 +26,9 @@ class AutoEntityLabelManagerTest extends TestCase {
    * @dataProvider providerAuto_entitylabel_entity_label_visible
    */
   public function testAuto_entitylabel_entity_label_visible(string $message, string $input, bool $expected) {
+    // This is a static pure method, so we don't have to instantiate
+    // AutoEntityLabelManager to call it. Good thing it's a pure method though
+    // because static methods don't allow us to use mocking.
     $output = AutoEntityLabelManager::auto_entitylabel_entity_label_visible($input);
 
     if ($output != $expected) {
@@ -59,6 +62,37 @@ class AutoEntityLabelManagerTest extends TestCase {
         'expected' => TRUE,
       ],
     ];
+  }
+
+  /**
+   * Test for getConfig().
+   *
+   * @cover ::getConfig
+   */
+  public function testGetConfig() {
+    $object = $this->getMockBuilder(AutoEntityLabelManager::class)
+      // NULL = no methods are mocked; otherwise list the methods here.
+      ->setMethods(NULL)
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $object->config = new class {
+      public function get(string $x) {
+        return 'hello';
+      }
+    };
+
+    $expected = 'hello';
+    $output = $object->getConfig('whatever');
+
+    if ($output != $expected) {
+      print_r([
+        'output' => $output,
+        'expected' => $expected,
+      ]);
+    }
+
+    $this->assertTrue($output == $expected, 'getConfig() works as expected if the config property is set.');
   }
 
 }
